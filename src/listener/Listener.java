09362ae287ede0b01220c44e0d778af3d46fc5db
Or Shwartz <Or Shwartz@localhost>
2010-04-10 00:46:03 +0300

    Added a few interfaces for sub-systems.

---
+++ src/listener/Listener.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package listener;
+
+/**
+ * @author Or Shwartz
+ *
+ */
+public interface Listener {
+
+	public boolean activate();
+	public boolean deactivate();
+	/* TBD remember to use Observer interface and Observable class */
+	public boolean listenTo(/*TBD Decide of format for parameter - 
+	 						container, single parameter, etc... - and
+	 						probably enable a recursive folder listening
+	 						parameter*/);
+	public boolean stopListeningTo(/*TBD same as above*/);
+}
