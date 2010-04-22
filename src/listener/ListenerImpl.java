f8a03ec96c0244cf56b41c626ca948bedf4ba762
Or Shwartz <Or Shwartz@localhost>
2010-04-22 22:23:10 +0300

    Implement a listener for filesystem as Observable.

---
+++ src/listener/ListenerImpl.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package listener;
+
+import java.util.Observable;
+
+/**
+ * @author Or Shwartz
+ *
+ */
+public class ListenerImpl extends Observable implements Listener {
+
+	@Override
+	public void activate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deactivate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isActive() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void listenTo(ListenedDirectory dir) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stopListeningTo(ListenedDirectory dir) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
