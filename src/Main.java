ad76eb077777e9412c28039201521186667b80cb
Or Shwartz <Or Shwartz@localhost>
2010-04-22 22:18:18 +0300

    Remove original test message.

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		// Welcome the user with a smile
-		System.out.println("Welcome to Tig-Tag-Toe :)");
+		
 	}
 
 }
