64cbee61026b0d93cf2902a33bf4a6b2f010d9b6
Or Shwartz <Or Shwartz@localhost>
2010-04-23 11:51:43 +0300

    Create basic command objects.

---
+++ src/commander/RequestTagCommand.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package commander;
+
+/**
+ * This command turns to the GUI for displaying a request to the user
+ * to tag a certain file.
+ * @author Or Shwartz
+ */
+public class RequestTagCommand implements Command {
+
+	/**
+	 * @see commander.Command#execute()
+	 */
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
