1f3e66eef6c30d9604506779a41d6c1e532e6aa4
Or Shwartz <Or Shwartz@localhost>
2010-04-23 13:45:00 +0300

    Make listener inheritance/implementation hierarchy tree more reasonable and simple.

--- src/listener/Listener.java
+++ src/listener/Listener.java
@@ -1,15 +1,18 @@
 package listener;
 
-/**
- * Interface for a filesystem listener, watching for changes.
- * @author Or Shwartz
- */
-public interface Listener {
+import java.util.Observable;
 
-	public void activate(); /* TBD throws exception? */
-	public void deactivate(); /* TBD throws exception? */
-	public boolean isActive();
+/**
+ * Abstract class for a filesystem listener, watching for changes.
+ * @see java.util.Observable
+ * @author Or Shwartz, Itay Evron
+ */
+public abstract class Listener extends Observable {
+
+	public abstract void activate(); /* TBD throws exception? */
+	public abstract void deactivate(); /* TBD throws exception? */
+	public abstract boolean isActive();
 	/* TBD remember to use Observer interface and Observable class */
-	public void listenTo(ListenedDirectory dir); /* TBD throws exception? */
-	public void stopListeningTo(ListenedDirectory dir); /* TBD throws exception? */
+	public abstract void listenTo(ListenedDirectory dir); /* TBD throws exception? */
+	public abstract void stopListeningTo(ListenedDirectory dir); /* TBD throws exception? */
 }
