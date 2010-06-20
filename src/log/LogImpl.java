bd162fab8776079cdd2d3e563e1046871dc3a1dc
Or Shwartz <shwartz@slimail.com>
2010-06-20 16:21:16 +0300

    Log prototype (using Log4J).

--- src/log/LogImpl.java
+++ src/log/LogImpl.java
@@ -1,6 +1,22 @@
 package log;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.SortedSet;
+import java.util.TreeSet;
+
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.xml.DOMConfigurator;
 
 /**
  * This is an implementation of a log. It's a singleton, so there
@@ -15,6 +31,9 @@ import java.util.SortedSet;
  */
 public class LogImpl implements Log {
 
+	private static final String LOG_NAME = "FileTag.log";
+	Logger logger;
+	
 	/**
 	 * 
 	 */
@@ -22,6 +41,10 @@ public class LogImpl implements Log {
 
 		// TODO: Remove this stub message
 		System.out.println(this.getClass().getName() + " up.");
+		
+		// Configure the Log4J
+		DOMConfigurator.configure("Log4j.xml");
+		logger = Logger.getLogger("FileTagger");
 	}
 	
 	/**
@@ -48,26 +71,80 @@ public class LogImpl implements Log {
 	 */
 	@Override
 	public void clearLog() {
-		// TODO Auto-generated method stub
-
+		
+		// Disable the log file
+		logger.getAppender("fileTag").close();
+		logger.removeAppender("fileTag");
+		
+		// Delete the log file
+		try {
+			File logFile = new File(LOG_NAME);
+			OutputStream os = new FileOutputStream(logFile);
+			OutputStreamWriter writer = new OutputStreamWriter(os);
+			writer.write("");
+			writer.close();
+		} catch (Exception e) {
+			// TODO: Something
+		}
+		
+		// Recreate the log file
+		try {
+			logger.addAppender(new FileAppender(new PatternLayout(), LOG_NAME, true));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	/**
 	 * @see log.Log#getMessages()
 	 */
 	@Override
-	public SortedSet<LogEvent> getMessages() {
-		// TODO Auto-generated method stub
-		return null;
+	public SortedSet<String> getMessages() {
+		
+		SortedSet<String> messages = new TreeSet<String>();
+		
+		// Read the log file line by line
+		InputStream inputStream = null;
+		
+		try {
+			inputStream = new FileInputStream(LOG_NAME);
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+		InputStreamReader logReader = new InputStreamReader(inputStream);
+		BufferedReader reader = new BufferedReader(logReader);
+		
+		String currLine;
+		
+		try {
+			currLine = reader.readLine();
+			
+			while (currLine != null) {
+						
+				messages.add(currLine);	
+				currLine = reader.readLine();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	
+		// Return the log messages
+		return messages;
 	}
 
 	/**
 	 * @see log.Log#writeMessage(log.LogEvent)
 	 */
 	@Override
-	public void writeMessage(LogEvent event) {
-		// TODO Auto-generated method stub
-
+	public void writeMessage(String message) {
+		
+		// Write the message to the main log file, using Log4J
+		logger.info(message);
+		
 	}
 
 }
