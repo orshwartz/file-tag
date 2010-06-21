45a19823a3af74438e30ce6b2f929ff1fea5ad00
Itai Evron <Itai Evron@localhost>
2010-06-21 17:52:54 +0300

    ..

--- src/tagger/TagRepository.java
+++ src/tagger/TagRepository.java
@@ -18,7 +18,7 @@ public interface TagRepository {
 	public void tagFile(String file, Collection<String> tags) throws FileNotFoundException;
 	public void untagFile(String file, String tag) throws FileNotTaggedException;
 	public void renameTag(String oldName, String newName) throws TagNotFoundException, TagAlreadyExistsException;
-	public Collection<File> searchByTag(Collection<String> includedTags,
+	public Collection<String> searchByTag(Collection<String> includedTags,
 										Collection<String> excludedTags);
 	public Collection<String> getTagListFreqOrdered();
 	
