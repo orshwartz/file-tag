8455d194298d92d2a5e8097f160a5e5444285d94
Or Shwartz <shwartz@slimail.com>
2010-06-21 18:04:51 +0300

    Add control for selecting folders - with checkboxes.

---
+++ src/gui/CheckBoxTreeViewer.java
@@ -0,0 +1,101 @@
+package gui;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jface.viewers.CheckStateChangedEvent;
+import org.eclipse.jface.viewers.CheckboxTreeViewer;
+import org.eclipse.jface.viewers.ICheckStateListener;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.LabelProviderChangedEvent;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.window.ApplicationWindow;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+class FileTree extends ApplicationWindow {
+	/**
+	 * FileTree constructor
+	 */
+	public FileTree() {
+		super(null);
+	}
+
+	/**
+	 * Runs the application
+	 */
+	public void run() {
+		// Don't return from open() until window closes
+		setBlockOnOpen(true);
+
+		// Open the main window
+		open();
+
+		// Dispose the display
+		Display.getCurrent().dispose();
+	}
+
+	/**
+	 * Configures the shell
+	 * 
+	 * @param shell
+	 *            the shell
+	 */
+	protected void configureShell(Shell shell) {
+		super.configureShell(shell);
+
+		// Set the title bar text and the size
+		shell.setText("File Tree");
+		shell.setSize(400, 400);
+	}
+
+	/**
+	 * Creates the main window's contents
+	 * 
+	 * @param parent
+	 *            the main window
+	 * @return Control
+	 */
+	protected Control createContents(Composite parent) {
+		Composite composite = new Composite(parent, SWT.NONE);
+		composite.setLayout(new GridLayout(1, false));
+
+		// Add a checkbox to toggle whether the labels preserve case
+		Button preserveCase = new Button(composite, SWT.CHECK);
+		preserveCase.setText("&Preserve case");
+
+		// Create the tree viewer to display the file tree
+		final TreeViewer tv = new TreeViewer(composite);
+		tv.getTree().setLayoutData(new GridData(GridData.FILL_BOTH));
+		tv.setContentProvider(new FileTreeContentProvider());
+		tv.setLabelProvider(new FileTreeLabelProvider());
+		tv.setInput("root"); // pass a non-null that will be ignored
+
+		// When user checks the checkbox, toggle the preserve case attribute
+		// of the label provider
+		preserveCase.addSelectionListener(new SelectionAdapter() {
+			public void widgetSelected(SelectionEvent event) {
+				boolean preserveCase = ((Button) event.widget).getSelection();
+				FileTreeLabelProvider ftlp = (FileTreeLabelProvider) tv
+						.getLabelProvider();
+				ftlp.setPreserveCase(preserveCase);
+			}
+		});
+		return composite;
+	}
+}
