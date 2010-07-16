1e602d4e0b2581d225b8f9ba0f7065937cf0e64b
Or Shwartz <shwartz@slimail.com>
2010-07-16 17:44:36 +0300

    Set correct order of table/view drops - so it works.

--- cleanUp.sql
+++ cleanUp.sql
@@ -1,5 +1,6 @@
 connect 'jdbc:derby://localhost:1527/myDB;create=true;user=me;password=mine;';
 
+drop view freq_desc_tags;
+drop table attachments;
 drop table tags;
 drop table files;
-drop table attachments;
