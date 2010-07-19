bbaf473504518d7d28d3784b34cb0cbb089b07a2
Or Shwartz <shwartz@slimail.com>
2010-07-19 15:53:31 +0300

    TagFreq now implements Comparable<TagFreq> for easy sorting by frequency.

--- src/tagger/TagFreq.java
+++ src/tagger/TagFreq.java
@@ -1,39 +1,46 @@
 package tagger;
 
-public class TagFreq implements Comparable {
+/**
+ * This class represents the frequency of a tag. It contains the tag and
+ * its occurrence count.
+ * @author Or Shwartz
+ */
+public class TagFreq implements Comparable<TagFreq> {
 
 	private int freq;
 	private String tagName;
-	
-	public TagFreq(int freq, String tagName){
+
+	public TagFreq(int freq, String tagName) {
 		this.freq = freq;
-		this.tagName = tagName;	
-	}
-	
-	
-	public void setFreq(int freq){
-		this.freq = freq;
-	}
-	
-	public int getFreq(){
-		return freq;
-	}
-	
-	
-	public void setTagName(String tagName){
 		this.tagName = tagName;
 	}
-	
-	public String getTagName(){
+
+	public void setFreq(int freq) {
+		this.freq = freq;
+	}
+
+	public int getFreq() {
+		return freq;
+	}
+
+	public void setTagName(String tagName) {
+		this.tagName = tagName;
+	}
+
+	public String getTagName() {
 		return tagName;
 	}
-	
-	
+
+	/**
+	 * Returns a negative integer if this is less frequent than the other.
+	 * 0 is returned if the frequencies are equal and a positive integer
+	 * is returned if this is more frequent than the other.
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
 	@Override
-	public int compareTo(Object arg0) {
-		// TODO Auto-generated method stub
-		return 0;
+	public int compareTo(TagFreq otherTagFreq) {
+		
+		// Return the comparison result 
+		return this.getFreq() - otherTagFreq.getFreq();
 	}
-	
-	
 }
