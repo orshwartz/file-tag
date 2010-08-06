aa26c4ca81000eb51669ff10d25f146a27049b88
Or Shwartz <shwartz@slimail.com>
2010-08-06 11:00:50 +0300

    Enable basic searching (at least, gets the tags to display).

--- src/gui/SearchWindow.java
+++ src/gui/SearchWindow.java
@@ -1,12 +1,22 @@
 package gui;
 
+import static commander.CommandManager.CmdCodes.TAGGER_GET_FILES_BY_TAGS;
+import static commander.CommandManager.CmdCodes.TAGGER_GET_TAGS_BY_FREQ;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Button;
-import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
 
+import tagger.TagFreq;
+
 import commander.CommandManager;
+import commander.commands.TSCommand;
 
 /**
 * This code was edited or generated using CloudGarden's Jigloo
@@ -22,7 +32,6 @@ import commander.CommandManager;
 */
 /**
  * @author Itay Evron
- * 
  */
 public class SearchWindow {
 
@@ -32,24 +41,36 @@ public class SearchWindow {
 	private Button btnIncludeTag;
 	private Button btnSearch;
 	private List lstResults;
-	private Button btnExclude;
+	private Button btnExcludeTag;
 	private List lstIncludedTags;
 
 	private static Shell window;
-	private static Group group;
-
 	public SearchWindow(CommandManager commander) {
 
 		this.commander = commander;
 	}
 
+	@SuppressWarnings("unchecked")
 	public void makeWindow() {
 
 		window = new Shell(MainAppGUI.display, SWT.CLOSE | SWT.TITLE | SWT.MIN);
 		window.setText("Search");
 		{
-			lstAvailableTags = new List(window, SWT.NONE);
+			lstAvailableTags = new List(window, SWT.MULTI);
 			lstAvailableTags.setBounds(288, 9, 193, 222);
+
+			// Populate list of available tags, sorted by frequency
+			TSCommand getTagsCmd =
+				commander.getCommand(TAGGER_GET_TAGS_BY_FREQ);
+			Collection<TagFreq> availableTags =
+				(Collection<TagFreq>) getTagsCmd.execute(null);
+
+			// For each tag frequency
+			for (TagFreq tagFreq : availableTags) {
+				
+				// Add as string to list
+				lstAvailableTags.add(tagFreq.toString());
+			}
 		}
 		{
 			lstIncludedTags = new List(window, SWT.NONE);
@@ -63,16 +84,79 @@ public class SearchWindow {
 			btnIncludeTag = new Button(window, SWT.PUSH | SWT.CENTER);
 			btnIncludeTag.setText("<< Include");
 			btnIncludeTag.setBounds(211, 105, 69, 30);
+			btnIncludeTag.addListener(SWT.Selection, new Listener() {
+
+				@Override
+				public void handleEvent(Event arg0) {
+					// TODO Auto-generated method stub
+					
+					// Get indices of tag selections
+					String[] selTags =
+						lstAvailableTags.getSelection();
+					
+					// For each tag
+					for (String curTag : selTags) {
+						
+						// Add tag to included tags list
+						lstIncludedTags.add(new TagFreq(curTag).getTagName());
+						
+						// Remove tag from available tags list
+						lstAvailableTags.remove(curTag);
+					}
+				}
+			});
 		}
 		{
-			btnExclude = new Button(window, SWT.PUSH | SWT.CENTER);
-			btnExclude.setText("Exclude >>");
-			btnExclude.setBounds(490, 105, 69, 30);
+			btnExcludeTag = new Button(window, SWT.PUSH | SWT.CENTER);
+			btnExcludeTag.setText("Exclude >>");
+			btnExcludeTag.setBounds(490, 105, 69, 30);
+			btnExcludeTag.addListener(SWT.Selection, new Listener() {
+
+				@Override
+				public void handleEvent(Event arg0) {
+					// TODO Auto-generated method stub
+					
+					// Get tag selections
+					String[] selTags =
+						lstAvailableTags.getSelection();
+					
+					// For each tag
+					for (String curTag : selTags) {
+						
+						// Add tag to excluded tags list
+						lstExcludedTags.add(new TagFreq(curTag).getTagName());
+						
+						// Remove tag from available tags list
+						lstAvailableTags.remove(curTag);
+					}
+				}});
 		}
 		{
 			btnSearch = new Button(window, SWT.PUSH | SWT.CENTER);
 			btnSearch.setText("Search");
 			btnSearch.setBounds(354, 243, 60, 30);
+			btnSearch.addListener(SWT.Selection, new Listener() {
+
+				@Override
+				public void handleEvent(Event arg0) {
+					
+					// Clear previous search results
+					lstResults.removeAll();
+					
+					Object[] params = {Arrays.asList(lstIncludedTags.getItems()),
+									   Arrays.asList(lstExcludedTags.getItems())};
+
+					// Get search results
+					Collection<String> resultsCollection =
+						(Collection<String>)commander.getCommand(TAGGER_GET_FILES_BY_TAGS).
+						execute(params);
+
+					// Set results
+					for (String curFile : resultsCollection) {
+						lstResults.add(curFile);
+					}
+				}
+			});
 		}
 		{
 			lstResults = new List(window, SWT.NONE);
@@ -82,6 +166,7 @@ public class SearchWindow {
 
 	public void open() {
 
+		makeWindow();
 		window.open();
 		while (!window.isDisposed()) {
 			if (!MainAppGUI.display.readAndDispatch())
