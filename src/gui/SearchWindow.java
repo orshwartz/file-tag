255eade3473ab033a4155093bc1eabad161a1aa0
Or Shwartz <shwartz@slimail.com>
2010-09-05 04:01:05 +0300

    Make window better looking and resizable.

--- src/gui/SearchWindow.java
+++ src/gui/SearchWindow.java
@@ -7,8 +7,11 @@ import java.util.Arrays;
 import java.util.Collection;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
@@ -40,6 +43,10 @@ public class SearchWindow {
 	private List lstExcludedTags;
 	private Button btnIncludeTag;
 	private Button btnSearch;
+	private Label lblSearchResults;
+	private Label lblExcludedTags;
+	private Label lblAvailableTags;
+	private Label lblIncludedTags;
 	private List lstResults;
 	private Button btnExcludeTag;
 	private List lstIncludedTags;
@@ -53,42 +60,60 @@ public class SearchWindow {
 	@SuppressWarnings("unchecked")
 	public void makeWindow() {
 
-		window = new Shell(MainAppGUI.display, SWT.CLOSE | SWT.TITLE | SWT.MIN);
+		window = new Shell(MainAppGUI.display, SWT.CLOSE |
+											   SWT.TITLE |
+											   SWT.MAX |
+											   SWT.MIN |
+											   SWT.RESIZE);
+		GridLayout windowLayout = new GridLayout();
+		windowLayout.makeColumnsEqualWidth = false;
+		windowLayout.numColumns = 5;
+		window.setLayout(windowLayout);
 		window.setText("Search");
 		{
-			lstAvailableTags = new List(window, SWT.MULTI);
-			lstAvailableTags.setBounds(288, 9, 193, 222);
-
-			// Populate list of available tags, sorted by frequency
-			TSCommand getTagsCmd =
-				commander.getCommand(TAGGER_GET_TAGS_BY_FREQ);
-			Collection<TagFreq> availableTags =
-				(Collection<TagFreq>) getTagsCmd.execute(null);
-
-			// For each tag frequency
-			for (TagFreq tagFreq : availableTags) {
-				
-				// Add as string to list
-				lstAvailableTags.add(tagFreq.toString());
-			}
+			lblIncludedTags = new Label(window, SWT.NONE);
+			GridData lblIncludedTagsLData = new GridData();
+			lblIncludedTagsLData.horizontalSpan = 2;
+			lblIncludedTags.setLayoutData(lblIncludedTagsLData);
+			lblIncludedTags.setText("Included tags");
 		}
 		{
-			lstIncludedTags = new List(window, SWT.NONE);
+			lblAvailableTags = new Label(window, SWT.NONE);
+			GridData lblAvailableTagsLData = new GridData();
+			lblAvailableTagsLData.horizontalSpan = 2;
+			lblAvailableTags.setLayoutData(lblAvailableTagsLData);
+			lblAvailableTags.setText("Available tags");
+		}
+		{
+			lblExcludedTags = new Label(window, SWT.NONE);
+			GridData lblExcludedTagsLData = new GridData();
+			lblExcludedTags.setLayoutData(lblExcludedTagsLData);
+			lblExcludedTags.setText("Excluded tags");
+		}
+		{
+			lstIncludedTags = new List(window, SWT.BORDER);
+			GridData lstIncludedTagsLData = new GridData();
+			lstIncludedTagsLData.verticalAlignment = GridData.FILL;
+			lstIncludedTagsLData.grabExcessVerticalSpace = true;
+			lstIncludedTagsLData.grabExcessHorizontalSpace = true;
+			lstIncludedTagsLData.horizontalAlignment = GridData.FILL;
+			lstIncludedTags.setLayoutData(lstIncludedTagsLData);
 			lstIncludedTags.setBounds(12, 9, 193, 222);
-		}
-		{
-			lstExcludedTags = new List(window, SWT.NONE);
-			lstExcludedTags.setBounds(568, 7, 193, 227);
 		}
 		{
 			btnIncludeTag = new Button(window, SWT.PUSH | SWT.CENTER);
 			btnIncludeTag.setText("<< Include");
+			GridData btnIncludeTagLData = new GridData();
+			btnIncludeTagLData.horizontalAlignment = GridData.CENTER;
+			btnIncludeTagLData.widthHint = 69;
+			btnIncludeTagLData.heightHint = 30;
+			btnIncludeTag.setLayoutData(btnIncludeTagLData);
 			btnIncludeTag.setBounds(211, 105, 69, 30);
+			btnIncludeTag.setEnabled(false);
 			btnIncludeTag.addListener(SWT.Selection, new Listener() {
-
+				
 				@Override
 				public void handleEvent(Event arg0) {
-					// TODO Auto-generated method stub
 					
 					// Get indices of tag selections
 					String[] selTags =
@@ -103,18 +128,64 @@ public class SearchWindow {
 						// Remove tag from available tags list
 						lstAvailableTags.remove(curTag);
 					}
+					
+					// Enable search button
+					btnSearch.setEnabled(true);
+				}
+			});
+		}
+		{
+			lstAvailableTags = new List(window, SWT.MULTI | SWT.BORDER);
+			GridData lstAvailableTagsLData = new GridData();
+			lstAvailableTagsLData.verticalAlignment = GridData.FILL;
+			lstAvailableTagsLData.grabExcessVerticalSpace = true;
+			lstAvailableTagsLData.grabExcessHorizontalSpace = true;
+			lstAvailableTagsLData.horizontalAlignment = GridData.FILL;
+			lstAvailableTags.setLayoutData(lstAvailableTagsLData);
+			lstAvailableTags.setBounds(288, 9, 193, 222);
+			
+			// Populate list of available tags, sorted by frequency
+			TSCommand getTagsCmd =
+				commander.getCommand(TAGGER_GET_TAGS_BY_FREQ);
+			Collection<TagFreq> availableTags =
+				(Collection<TagFreq>) getTagsCmd.execute(null);
+			
+			// For each tag frequency
+			for (TagFreq tagFreq : availableTags) {
+				
+				// Add as string to list
+				lstAvailableTags.add(tagFreq.toString());
+			}
+			
+			lstAvailableTags.addListener(SWT.Selection, new Listener() {
+
+				@Override
+				public void handleEvent(Event arg0) {
+					
+					// If anything is selected
+					if (lstAvailableTags.getSelectionCount() >= 1) {
+						
+						// Enable inclusion and exclusion buttons
+						btnIncludeTag.setEnabled(true);
+						btnExcludeTag.setEnabled(true);
+					}
 				}
 			});
 		}
 		{
 			btnExcludeTag = new Button(window, SWT.PUSH | SWT.CENTER);
 			btnExcludeTag.setText("Exclude >>");
+			GridData btnExcludeTagLData = new GridData();
+			btnExcludeTagLData.horizontalAlignment = GridData.CENTER;
+			btnExcludeTagLData.widthHint = 69;
+			btnExcludeTagLData.heightHint = 30;
+			btnExcludeTag.setLayoutData(btnExcludeTagLData);
 			btnExcludeTag.setBounds(490, 105, 69, 30);
+			btnExcludeTag.setEnabled(false);
 			btnExcludeTag.addListener(SWT.Selection, new Listener() {
-
+				
 				@Override
 				public void handleEvent(Event arg0) {
-					// TODO Auto-generated method stub
 					
 					// Get tag selections
 					String[] selTags =
@@ -129,12 +200,30 @@ public class SearchWindow {
 						// Remove tag from available tags list
 						lstAvailableTags.remove(curTag);
 					}
+					
+					// Enable search button
+					btnSearch.setEnabled(true);
 				}});
+		}
+		{
+			lstExcludedTags = new List(window, SWT.BORDER);
+			GridData lstExcludedTagsLData = new GridData();
+			lstExcludedTagsLData.verticalAlignment = GridData.FILL;
+			lstExcludedTagsLData.grabExcessVerticalSpace = true;
+			lstExcludedTagsLData.grabExcessHorizontalSpace = true;
+			lstExcludedTagsLData.horizontalAlignment = GridData.FILL;
+			lstExcludedTags.setLayoutData(lstExcludedTagsLData);
+			lstExcludedTags.setBounds(568, 7, 193, 227);
 		}
 		{
 			btnSearch = new Button(window, SWT.PUSH | SWT.CENTER);
 			btnSearch.setText("Search");
+			GridData btnSearchLData = new GridData();
+			btnSearchLData.horizontalSpan = 5;
+			btnSearchLData.horizontalAlignment = GridData.CENTER;
+			btnSearch.setLayoutData(btnSearchLData);
 			btnSearch.setBounds(354, 243, 60, 30);
+			btnSearch.setEnabled(false);
 			btnSearch.addListener(SWT.Selection, new Listener() {
 
 				@Override
@@ -159,9 +248,26 @@ public class SearchWindow {
 			});
 		}
 		{
-			lstResults = new List(window, SWT.NONE);
+			lblSearchResults = new Label(window, SWT.NONE);
+			GridData lblSearchResultsLData = new GridData();
+			lblSearchResultsLData.horizontalSpan = 5;
+			lblSearchResults.setLayoutData(lblSearchResultsLData);
+			lblSearchResults.setText("Search results");
+		}
+		{
+			lstResults = new List(window, SWT.BORDER);
+			GridData lstResultsLData = new GridData();
+			lstResultsLData.horizontalSpan = 5;
+			lstResultsLData.horizontalAlignment = GridData.FILL;
+			lstResultsLData.verticalAlignment = GridData.FILL;
+			lstResultsLData.grabExcessVerticalSpace = true;
+			lstResultsLData.grabExcessHorizontalSpace = true;
+			lstResults.setLayoutData(lstResultsLData);
 			lstResults.setBounds(12, 282, 749, 182);
 		}
+
+		window.setSize(500, 400);
+		window.setMinimumSize(400, 200);
 	}
 
 	public void open() {
@@ -172,6 +278,5 @@ public class SearchWindow {
 			if (!MainAppGUI.display.readAndDispatch())
 				MainAppGUI.display.sleep();
 		}
-		//pack();
 	}
 }
