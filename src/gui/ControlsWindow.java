0ae309a4ce97b82319d85759f170ed74764f3cb7
Itai Evron <Itai Evron@localhost>
2010-09-25 16:13:13 +0200

    (no commit message)

--- src/gui/ControlsWindow.java
+++ src/gui/ControlsWindow.java
@@ -4,18 +4,36 @@ import static commander.CommandManager.CmdCodes.LSTNR_ACTIVATE;
 import static commander.CommandManager.CmdCodes.LSTNR_DEACTIVATE;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Table;
 
 import com.cloudgarden.resource.SWTResourceManager;
 import commander.CommandManager;
 
+
+/**
+* This code was edited or generated using CloudGarden's Jigloo
+* SWT/Swing GUI Builder, which is free for non-commercial
+* use. If Jigloo is being used commercially (ie, by a corporation,
+* company or business for any purpose whatever) then you
+* should purchase a license for each developer using Jigloo.
+* Please visit www.cloudgarden.com for details.
+* Use of Jigloo implies acceptance of these licensing terms.
+* A COMMERCIAL LICENSE HAS NOT BEEN PURCHASED FOR
+* THIS MACHINE, SO JIGLOO OR THIS CODE CANNOT BE USED
+* LEGALLY FOR ANY CORPORATE OR COMMERCIAL PURPOSE.
+*/
 /**
  * 
  * @author Itai Evron
@@ -25,107 +43,54 @@ public class ControlsWindow {
 
 	public CommandManager commander;
 	private static Shell window;
-	private static Group group, group2;
-	private boolean listenerOnOff;
-	private boolean logOnOff;
 	
-	private List log;
 	
 	private static Group logGroup;
-	
+	private Table table1;
+	private Label label1;
+	private Button button1;
+
 	public ControlsWindow(CommandManager commander){
 		this.commander = commander;	
-		listenerOnOff = false;
-		logOnOff = false;
+		
 	}
 	
 	public void makeWindow(){
 		// make the window
-		window = new Shell(MainAppGUI.display);
+		window = new Shell(MainAppGUI.display, SWT.DIALOG_TRIM | SWT.APPLICATION_MODAL);
 		window.setText("Control and Monitor");
 		//GridLayout sShellLayout = new GridLayout();
 		//sShellLayout.makeColumnsEqualWidth = true;
 		//sShellLayout.numColumns = 2;
-		window.setLayout(new GridLayout(2,true));
+		window.setLayout(new FormLayout());
+		window.setMaximized(false);
 		window.setSize(500, 340);
-		
-		group=new Group(window,SWT.SHADOW_IN);
-    	group.setLayout(new GridLayout(2,false));
-    	group.setLayoutData(new GridData(SWT.FILL,SWT.CENTER,true,false));
-    	
-    	group2=new Group(window,SWT.SHADOW_IN);
-    	group2.setLayout(new GridLayout(2,false));
-    	group2.setLayoutData(new GridData(SWT.FILL,SWT.CENTER,true,false));
-    	
-    	/* ---------------------- Parts --------------------------------*/
-    	
-    	final Button logBtn = new Button(group, SWT.PUSH);
-    	GridData logBtnData = new GridData();
-    	logBtnData.widthHint = 203;
-    	logBtnData.heightHint = 68;
-		logBtn.setLayoutData(logBtnData);
-    	logBtn.setText("Present Log");
-    	logBtn.setImage(SWTResourceManager.getImage("gui/res/b1eye011.PNG"));
-    	
-    	
-    	final Button lstBtn = new Button(group2, SWT.PUSH);
-    	lstBtn.setText("Activate Listener");
-    	GridData lstBtnData = new GridData();
-    	lstBtnData.widthHint = 213;
-    	lstBtnData.heightHint = 68;
-		lstBtn.setLayoutData(lstBtnData);
-    	lstBtn.setImage(SWTResourceManager.getImage("gui/res/Radio.PNG"));
-		
-    	
-    	
-    	
-    	
-    	/* Listen to listener button */
-    	
-    	lstBtn.addListener(SWT.Selection, new Listener() {
-    		public void handleEvent(Event e) {
-    			switch (e.type) {
-    	          case SWT.Selection:
-    	        	  if(listenerOnOff == false){
-    	        		  commander.runCommand(LSTNR_ACTIVATE, null);
-    	        		  listenerOnOff = true;
-    	        		  lstBtn.setText("Deactivate Listener");
-    	        		  
-    	        	  }
-    	        	  else{
-    	        		  commander.runCommand(LSTNR_DEACTIVATE, null);
-    	        		  listenerOnOff = false;
-    	        		  lstBtn.setText("Activate Listener");
-    	        	  }
-    	   
-    			}	
-    		   }
-    	});
-    	
-    	
-    	/* Listen to log button */
-    	
-    	logBtn.addListener(SWT.Selection, new Listener() {
-    		public void handleEvent(Event e) {
-    			switch (e.type) {
-    	          case SWT.Selection:
-    	        	  if(logOnOff == false){
-    	        		  window.setSize(500,640);
-    	        		  logOnOff = true;
-    	        		  logBtn.setText("Hide Log");
-    	        	  }
-    	        	  
-    	        	  else{
-    	        		  window.setSize(500,340);
-    	        		  logOnOff = false;
-    	        		  logBtn.setText("Present Log");
-    	        	  }
-    	   
-    			}	
-    		   }
-    	});
-    	
-    	
+		{
+			label1 = new Label(window, SWT.NONE);
+			FormData label1LData = new FormData();
+			label1LData.left =  new FormAttachment(0, 1000, 12);
+			label1LData.top =  new FormAttachment(0, 1000, 12);
+			label1LData.width = 115;
+			label1LData.height = 13;
+			label1.setLayoutData(label1LData);
+			label1.setText("System Log");
+		}
+		{
+			FormData table1LData = new FormData();
+			table1LData.width = 330;
+			table1LData.height = 239;
+			table1 = new Table(window, SWT.BORDER);
+			table1.setLayoutData(table1LData);
+		}
+		{
+			button1 = new Button(window, SWT.PUSH | SWT.UP);
+			FormData button1LData = new FormData();
+			button1LData.width = 113;
+			button1LData.height = 49;
+			button1.setLayoutData(button1LData);
+			button1.setText("Activate Listener");
+		}
+
 	}
 	
 	
